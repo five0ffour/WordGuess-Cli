005721a8de44bcfa5e904e6e475885fd4bf24d5a
Mike Galarneau <mbgalarneau@gmail.com>
2018-12-05 19:24:35 -0500

    removed the bloody game object, not worth the trouble!

--- index.js
+++ index.js
@@ -1,92 +1,77 @@
 // * Randomly selects a word and uses the `Word` constructor to store it
 // * Prompts the user for each guess and keeps track of the user's remaining guessesvar inquirer = require('inquirer');
-
 var inquirer = require('inquirer');
-
 var WordTable = require("./wordtable.js");
 var Word = require("./word.js");
-
-var game = {
-
-    //-----------
-    // Variables 
-    //-----------
-    word: new Word(),
-    wordTable: new WordTable(),
-    maxRounds: 10,
-    currentRound: 0,
-
-    //-----------
-    // Methods
-    //-----------
-
-    resetGame: function () {
-        currentRound = 0;
-        word = new Word();
-        word.setWord(this.wordTable.getRandomWord());
-    },
-
-    playGame: function () {
-        console.log("\n--------\nplayGame()\n---------");
-        this.resetGame();
-
-        if (!this.isGameOver()) {
-            this.playRound();
-        } else {
-            this.endGame();
-        }
-    },
-
-    promptGuess: function () {
-        inquirer
-            .prompt([{
-                name: "letter",
-                type: "input",
-                message: "Guess a letter:"
-            }])
-            .then(function (answer) {
-                console.log("Your guess: ", answer.letter);
-
-                word.guessLetter(answer.letter);
-                console.log(word.getWord());
-            });
-    },
-
-    playRound: function () {
-
-        currentRound++;
-        console.log("\n----------\nplayRound: ROUND " + currentRound + "\n----------\n");
-        this.promptGuess();
-        word.getWord();
-    },
-
-    isGameOver: function () {
-        return (word.getWord() === word.original ||
-            currentRound >= this.maxRounds);
-    },
-
-    endGame: function () {
-        console.log("\n--------endGame()--------\n");
-
-        // Prompts the user if they would like to play again.
-        // Otherwise print the "come back again soon message" and exit
-        inquirer
-            .prompt({
-                name: "again",
-                type: "confirm",
-                message: "Would you like to play another game?"
-            })
-            .then(function (answer) {
-                if (answer.again === true) {
-                    playGame();
-                } else {
-                    console.log("Come back again soon!");
-                }
-            });
+//-------------------
+// Global Variables - 
+//-------------------
+var word = new Word();
+var wordTable = new WordTable();
+var currentRound = 0;
+var maxGuesses = 10;
+var guesses = 0;
+//-----------
+// Methods
+//-----------
+function resetGame() {
+    currentRound = 0;
+    word = new Word();
+    word.setWord(wordTable.getRandomWord());
+}
+function playGame() {
+    console.log("\n--------\nplayGame()\n---------");
+    resetGame();
+    playRound();
+}
+function promptGuess() {
+    inquirer
+        .prompt([{
+            name: "letter",
+            type: "input",
+            message: "Guess a letter:"
+        }])
+        .then(function (answer) {
+        console.log("Your guess: ", answer.letter);
+        word.guessLetter(answer.letter);
+        console.log(word.getWord());
+        playRound();
+    });
+}
+function playRound() {
+    currentRound++;
+    if (!isGameOver()) {
+        console.log("\n----------\nplayRound: Round " + currentRound + "\n----------\n");
+        promptGuess();
+    }
+    else {
+        endGame();
     }
 }
-
+function isGameOver() {
+    return (word.getWord() === word.original ||
+        guesses >= maxGuesses);
+}
+function endGame() {
+    console.log("\n--------Game Over--------\n");
+    // Prompts the user if they would like to play again.
+    // Otherwise print the "come back again soon message" and exit
+    inquirer
+        .prompt({
+        name: "again",
+        type: "confirm",
+        message: "Would you like to play another game?"
+    })
+        .then(function (answer) {
+        if (answer.again === true) {
+            playGame();
+        }
+        else {
+            console.log("Come back again soon!");
+        }
+    });
+}
 //---------------------
 // Main Start of Game - 
 //---------------------
-game.playGame();
+playGame();
--- letter.js
+++ letter.js
@@ -1,46 +1,34 @@
 // Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
-
 //   * A string value to store the underlying character for the letter
-
 //   * A boolean value that stores whether that letter has been guessed yet
-
 //   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-
 //   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-
-function Letter (letter) {
-
+function Letter(letter) {
+    var _this = this;
     this.value = letter;
     this.guessed = false;
-
-    this.isGuessed = () => {
-        return (this.guessed ? this.value : "_");
-    }
-
-    this.guessLetter = (character) => {
-        if ((character === ' ') || 
-            (character.toLowerCase() === this.value.toLowerCase())) {
-            this.guessed = true;
+    this.isGuessed = function () {
+        return (_this.guessed ? _this.value : "_");
+    };
+    this.guessLetter = function (character) {
+        if ((character === ' ') ||
+            (character.toLowerCase() === _this.value.toLowerCase())) {
+            _this.guessed = true;
         }
-    }
-
+    };
     // Test script for Letter
-    this.unitTest = () => {
-        console.log('Original value: ' + this.value);
-
-        guess.guessLetter('b');
-        console.log('Test fail condition: ' + this.isGuessed());
-
-        guess.guessLetter('a');
-        console.log('Test success condition: ' + this.isGuessed());
-    }
+    this.unitTest = function () {
+        console.log('Original value: ' + _this.value);
+        _this.guessLetter('b');
+        console.log('Test fail condition: ' + _this.isGuessed());
+        _this.guessLetter('a');
+        console.log('Test success condition: ' + _this.isGuessed());
+    };
 }
-
 //----------------------------------------
 // Uncomment for test script of Letter methods
 //----------------------------------------
 // var guess = new Letter('a');
 // guess.unitTest();
 //----------------------------------------
-
 module.exports = Letter;
